5849054be864a12f053324b7fe9c19354f8e6562
Maxime Kohlhaas <mko@atm-consulting.fr>
2013-04-23 08:30:13 +0200

    Work on address

--- modules/address/class/class.address.php
+++ modules/address/class/class.address.php
@@ -3,7 +3,7 @@ class TAddress extends TObjetStd {
 	function __construct() { 
 		parent::set_table('address');
 		//parent::add_champs('','type=entier;');
-		parent::add_champs('name,address1,address2,cp,city','type=chaine;');
+		parent::add_champs('name,address,zip,city','type=chaine;');
 		
 		TAtomic::initExtraFields($this);
 		
--- modules/address/config/config.php
+++ modules/address/config/config.php
@@ -3,4 +3,5 @@
 	$conf->modules[]=array(
 		'name'=>'Adresses'
 		,'id'=>'address'
+		,'class'=>array('TAddress')
 	);
