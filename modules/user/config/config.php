94de00f0464d332f99116ad9b545d8d8bfc9d116
unknown <alexis@.(none)>
2013-04-28 10:26:49 +0200

    add menu "manage group"

--- modules/user/config/config.php
+++ modules/user/config/config.php
@@ -17,8 +17,15 @@
 		,'url'=>HTTP.'modules/user/user.php'
 		,'right'=>array('user','all','view')
 	);
-
 	
+	$conf->menu->admin[] = array(
+		'name'=>"Manage groups"
+		,'id'=>'MGroups'
+		,'position'=>3
+		,'url'=>HTTP.'modules/user/group.php'
+		,'right'=>array('group','all','view')
+	);
+
 	$conf->modules['user']=array(
 		'name'=>'User'
 		,'id'=>'TUser'
@@ -29,4 +36,13 @@
 	$conf->rigths[]=array('user','all','edit');
 	$conf->rigths[]=array('user','me','view');
 	$conf->rigths[]=array('user','me','edit');
+	
+	$conf->tabs->TCompany[]=array(
+		'user'=>array('label'=>'__tr(User)__','url'=>HTTP.'modules/user/user.php.php?id=@id@')
+	);
+	
+	$conf->tabs->TGroup[]=array(
+		'group'=>array('label'=>'__tr(Card)__','url'=>HTTP.'modules/user/group.php.php?id=@id@')
+		,'user'=>array('label'=>'__tr(Users)__','url'=>HTTP.'modules/user/user.php.php?id_group=@id@')
+	);
 	
