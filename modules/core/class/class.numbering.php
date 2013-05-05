9d5c264fe40305b45e0d26a2dfc918b02dd0ca65
Maxime Kohlhaas <mko@atm-consulting.fr>
2013-05-05 20:08:52 +0200

    Add core class for numbering

---
+++ modules/core/class/class.numbering.php
@@ -0,0 +1,57 @@
+<?
+class TNumbering extends TObjetStd {
+	function __construct() {
+		
+		parent::set_table(DB_PREFIX.'numbering');
+		
+		parent::add_champs('id_entity','type=entier;index;');
+		parent::add_champs('numberValue','type=entier;index;');
+		parent::add_champs('module,mask','type=chaine;');
+		
+		TAtomic::initExtraFields($this);
+
+		parent::start();
+		parent::_init_vars();
+	}
+	
+	static function getNextRefValue(&$db, &$user, $module, $date, $mask='', $status=0) {
+		if($status == 0) {
+			//return '(DRAFT'.$object->getID().')';
+		}
+		if($mask == '') {
+			return $object->getID();
+		}
+		if(empty($date)) $date = time();
+		
+		$maskToSearch = $mask;
+		
+		// Remplacement des variables de date
+		$maskToSearch = preg_replace('|{yy}|', date('y', $date), $maskToSearch);
+		$maskToSearch = preg_replace('|{yyyy}|', date('Y', $date), $maskToSearch);
+		$maskToSearch = preg_replace('|{mm}|', date('m', $date), $maskToSearch);
+		$maskToSearch = preg_replace('|{dd}|', date('d', $date), $maskToSearch);
+		
+		$params = array(
+			'module' => $module
+			,'id_entity' => $user->id_entity
+			,'mask' => $maskToSearch
+		);
+		
+		$num = new TNumbering;
+		$TNum = TRequeteCore::get_id_from_what_you_want($db, $num->get_table(), $params);
+		if(!empty($TNum[0])) {
+			$num->load($TNum[0]);
+		} else {
+			$num->module = $module;
+			$num->id_entity = $user->id_entity;
+			$num->mask = $maskToSearch;
+			$num->numberValue = 0;
+		}
+		
+		$num->numberValue++;
+		$num->save($db);
+		$ref = preg_replace('|{0.*?}|', $num->numberValue, $maskToSearch);
+		
+		return $ref;
+	}
+}
